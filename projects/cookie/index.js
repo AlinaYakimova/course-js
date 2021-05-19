2c8110b510cd9f035004df0ab1495bfd41a6ed25
Alina Yakimova <komarovatz@gmail.com>
2021-05-19 22:33:17 +0300

    убрала очистку полей ввода куки

--- projects/cookie/index.js
+++ projects/cookie/index.js
@@ -78,9 +78,6 @@ addButton.addEventListener('click', () => {
   document.cookie = `${name}=${value}`;
   cookiesMap.set(name, value);
 
-  addNameInput.value = '';
-  addValueInput.value = '';
-
   updateTable();
 });
 
